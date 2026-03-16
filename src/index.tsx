7d3d8e1b22b33dcafc3ab3b18eb80fef502e64f5
xxf <zhengwutati@gmail.com>
2026-03-16 16:25:37 +0800

    feat:firstcommit

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { defineCustomElements } from '@ionic/pwa-elements/loader';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+defineCustomElements(window);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
